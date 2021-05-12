3fef8fb754c89fabd7b3db8fe6dd4c14f32b8c11
Glitch (glitch-hello-node) <none>
2021-05-12 18:30:10 +0000

    🌞🌚 Checkpoint ./komutlar/ping.js:30317047/17:31549834/327 ./ayarlar.json:31549834/27 ./server.js:30317047/34

--- komutlar/ping.js
+++ komutlar/ping.js
@@ -5,4 +5,19 @@ exports.run = async (client ,message, args) =>{
 
 const embed = new Discord.MessageEmbed()
 .setTitle("Pingim")
-.setColor("Random")
+.setColor("RANDOM")
+.setDesceription(` ms`)
+message.channel.send(embed)
+};
+exports.conf = {
+enabled: true,
+ guildOnly: false,
+ aliases: ['ping','ms'],
+ permLevel: 0
+};
+
+exports.help = {
+name: 'ping',
+description: 'Botun Pingine Bakarsınız',
+usage: 'ping'
+};
