62d635ac08fc4bb4cf5200783114a08dee1caffc
Glitch (glitch-hello-node) <none>
2021-05-16 10:55:25 +0000

    🐥🚏 Checkpoint ./komutlar/yardım.js:30317047/3127 ./package.json:30317047/2 ./komutlar/yardım2.js:30317047/4204

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -1,15 +1,20 @@
-const Discord = require('discord.js');
-const db = require('quick.db');
+const Discord = require('discord.js')
+
+const ayarlar = require('../ayarlar.json')
+
+let prefix = ayarlar.prefix
 
 exports.run = async (client, message, args) => { 
 
+  
 
-  let prefix = (await db.fetch(`prefix_${message.guild.id}`)) || "!";
   const embed = new Discord.MessageEmbed()
 
-let yardım = new Discord.MessageEmbed()  
 .setColor('RANDOM')
-.addField('Murat-Eren Yardım Menüsü',`
+  .setTitle('Murat-Eren Yardım Menüsü')
+
+  .setDescription(`
+
 ==============================================
 :hite_small_square: | **!test:** = botun çalıştığınız yada çalışmadığı söyler
 :white_small_square: | **!oylama:** = oyalma yaprsınız
@@ -29,43 +34,32 @@ let yardım = new Discord.MessageEmbed()
 :white_small_square: | **!türk:** = türk gif atar
 :white_small_square: | **!a101:** = :D
 :white_small_square: | **!yavaşmod:** = belittiğiniz sayıda kanalı yavaşlatır
-:white_small_square: | **!token:** = sana tokenimi veririm
-:white_small_square: | **!sunucubilgi:** = sunucuhakkında bilgi verir
-:white_small_square: | **!sil:** = belittiğiniz sayıda mesaj siler
-:white_small_square: | **!atatürk:** = o7
-:white_small_square: | **!avatar:** = avatarınızı gsterir
-:white_small_square: | **!botbilgi:** = botun bilgisini gösterir
-:white_small_square: | **!üyedurum:** = toplam üye sayısını göterir
-:white_small_square: | **!sunucuresmi:** = sunucunun resmini gösterir
-:white_small_square: | **!id:** = istediğiniz kişinin idsini verir
-:white_small_square: | **!kralol:** = kral olursun
-:white_small_square: | **!kedi:** = kedi gif 
-:white_small_square: | **!oy:** = oy linkimi veiririm ( kodlardan ayarla oy linkini)
-:white_small_square: | **!salak:** = salak gif atar :D
-:white_small_square: | **!korona :** = korona sayısını gösterir
-:white_small_square: | **!bansay:** = toplam ban sayısını gösterir
-:white_small_square: | **!yapımcım:** = yapımcımı gösterir
-:white_small_square: | **!aşkölçer:** = etiketleiğiniz kişiyle aşk derecenizi gösterir
-:white_small_square: | **!bug-bildir:** = botun yapımcısına bug bildirir
-:white_small_square: | **!mute:** = etiketlediğiniz kişiyi muteler
 ===============================================
-`)
-  .addField("**» Davet Linki**", " [Botu Davet Et](https://discord.com/api/oauth2/authorize?client_id=832200119133667360&permissions=8&scope=bot)", )
-    .setImage("https://cdn.discordapp.com/attachments/821825276232728576/821831310213382264/standard.gif")
-.setFooter(`${message.author.tag} Tarafından İstendi.`, message.author.avatarURL())
-.setThumbnail(client.user.avatarURL())
- message.channel.send(yardım) 
-  };
 
+Bağlantılar 
+**» Davet Linki** [Botu Davet Et](https://discord.com/api/oauth2/authorize?client_id=832200119133667360&permissions=8&scope=bot)
+`)
+
+ message.channel.send(embed) 
+}
 exports.conf = {
+
   enabled: true,
+
   guildOnly: false,
-  aliases: ['help'],
-  permLevel: 0
-};
+
+  permLevel: 0,
+
+  aliases: ["help"]
+
+}
 
 exports.help = {
-  name: "yardım",
-  category: "yardım",
-    description: "yardım"
-};
+
+  name: 'yardım',
+
+  description: 'Yardım Menüsünü Açar',
+
+  usage: 'yardım'
+
+}
