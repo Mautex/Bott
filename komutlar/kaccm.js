a5ffeac4470ba1e7858f3b8332acbb08c8491dce
Glitch (glitch-hello-node) <none>
2021-11-26 17:30:26 +0000

    🛢🎾 Checkpoint ./komutlar/kaccm.js:18220382/367

--- komutlar/kaccm.js
+++ komutlar/kaccm.js
@@ -3,18 +3,18 @@ const ayarlar = require('../ayarlar.json');
 const client = new Discord.Client();
 
 exports.run = (client, message) => {
-  if (message.author.id === ayarlar.sahipid) {
+  if (message.author.id === ayarlar.sahip) {
     message.channel.send('Reis o kadar kudretlisin ki olcemedik.')
-  }
-    else(
+  } else {
+  
    message.channel.send('Bir dakika abi...').then(message => {
    var espriler = [' **Senin Alet  18CM ** :eggplant: ' ,'**Senin Alet  11CM ** :eggplant:' ,'**Senin Alet 32CM  ** :eggplant:' ,'**Senin Alet  35CM ** :eggplant:' ,'**Senin Alet  8CM  ** :eggplant:' ,'**Senin Alet  65CM  ** :eggplant:' ,'**Senin Alet 5CM  ** :eggplant:' ,'**Senin Alet 31CM  ** :eggplant:' ,'**Senin Alet  14CM ** :eggplant:' ,'**Senin Alet  1CM ** :eggplant:'];
       var espri = espriler[Math.floor(Math.random() * espriler.length)];
-            message.edit(`${espri}`);
+          message.edit(`${espri}`);
  });
   }
-)
-
+  
+}
 exports.conf = {
   enabled: true,
   guildOnly: false,
