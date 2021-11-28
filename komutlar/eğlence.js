7af682c0ec851e929bf94bbceba21df348a53e87
Glitch (glitch-hello-node) <none>
2021-11-28 14:33:39 +0000

    🌮🚄 Checkpoint ./komutlar/yavaşmod.js:39190362/2 ./komutlar/eğlence.js:39190362/664 ./komutlar/korona.js:39190362/17107

--- komutlar/eğlence.js
+++ komutlar/eğlence.js
@@ -18,6 +18,7 @@ exports.run = async (client, message, args) => {
 :white_small_square: | **!atatürk:** = o7.
 :white_small_square: | **!avatar:** = avatarınızı gsterir.
 :white_small_square: | **!id:** = istediğiniz kişinin idsini verir.
+:white_small_square: | **!kaçcm:** = sen anladın orayı :D
 :white_small_square: | **!kralol:** = kral olursun.
 :white_small_square: | **!kedi:** = kedi gif.
 :white_small_square: | **!salak:** = salak gif atar :D.
@@ -25,7 +26,7 @@ exports.run = async (client, message, args) => {
 :white_small_square: | **!yapımcım:** = yapımcımı gösterir.
 :white_small_square: | **!aşkölçer:** = etiketleiğiniz kişiyle aşk derecenizi gösterir.
 :white_small_square: | **!espiri:** = espiri yaparsınız.
-:white_small_square: | **!kapaklaf:** = Bot kapaklaf söyler.
+:white_small_square: | **!lafat:** = Bot laf atar.
 :white_small_square: | **!türk:** = Bot türkiye gifi gösterir.
 :white_small_square: | **!ara155:** = 155'i arasınız.
 :white_small_square: | **!yazankazanır:** = ilk yazan kazanır :D
--- komutlar/korona.js
+++ komutlar/korona.js
@@ -3,47 +3,49 @@ const snekfetch = require('snekfetch');
 const ayarlar = require('../ayarlar.json');
 
 exports.run = async (client, message, args) => {
-  if(!args[0]) return message.channel.send(
-  new Discord.MessageEmbed()
-  .setDescription(`Bir ülke girmeyi unuttunuz!! Örnek: ${ayarlar.prefix}korona TR(TÜRKİYE) ,CH(CHINA), US(A.B.D), FR (FRANSA), NE(NORVEÇ), IS(İSVEÇ)`));
+  if(!args[0]) return message.reply('𝘾𝙊𝙍𝙊𝙉𝘼 𝙑𝙄𝙍𝙐𝙎 𝘼𝙉𝙇𝙄𝙆 𝙄𝙎𝙏𝘼𝙏𝙄𝙎𝙏𝙄𝙆 𝙏𝘼𝘽𝙇𝙊𝙎𝙐 𝙆𝙐𝙇𝙇𝘼𝙉𝙄𝙈```Güncel Covid-19 vaka istatistiklerini buradan görebilirsin. Ülkeye özel istatistikler için !korona <ülke kodu> komutunu kullanabilirsin. #evdekal,#evhayattır,#evdeyasamvar``` <a:evet:682967451540914351>**Doğru Kullanım:** __!korona <ülke kodu>__')
+
   let a = args[0].toLowerCase()
   .replace('türkiye', 'TR')
-  .replace('çin', 'CH')
+  .replace('çin', 'CN')
   .replace('amerika', 'US')
   .replace('japonya', 'JP')
   .replace('fransa', 'FR')
   .replace('norveç', 'NE')
-  .replace('isveç',  'IS')
-  
+  .replace('isveç',  'SN')
+ 
     const text = await snekfetch.get(`https://thevirustracker.com/free-api?countryTotal=${a}`);
     const body = text.body;
   let ülk = body.countrydata[0].info.title
 
-    let zerocode = new Discord.MessageEmbed()
-    .setColor('WHITE')
+    let embed = new Discord.RichEmbed()
+    .setColor('RED')
     .setTitle('COVID-19')
-    .setDescription(`COVID-19 İstatistikleri **${ülk}**`)
-    .setThumbnail('https://dfcby4322olzt.cloudfront.net/wp-content/uploads/2020/03/1800x1200_coronavirus_1.jpg')
-    .addField(' Toplam vaka sayısı:', `${body.countrydata[0].total_cases}`, false)
-    .addField(' İyileşen:', `${body.countrydata[0].total_recovered}`, false)
-    .addField(' Enfekte:', `${body.countrydata[0].total_active_cases}`, false)
-    .addField(' Ölümler:', `${body.countrydata[0].total_deaths}`, false)
-    .addField(' Bugün yeni vakalar:', `${body.countrydata[0].total_new_cases_today}`, false)
-    .addField(' Bugün yeni ölümler:', `${body.countrydata[0].total_new_deaths_today}`, false)
-    .addField(' Ciddi Vakalar:', `${body.countrydata[0].total_serious_cases}`, false)
+    .setDescription(`COVID-19 Statistics in **${ülk}**`)
+    .setThumbnail('https://www.diken.com.tr/wp-content/uploads/2020/01/corona_reuters-copy.jpg')
+    .addField('*:microbe: Toplam Vaka:*',`\`\`〘 → ${body.countrydata[0].total_cases} ← 〙\`\` `, true)
+    .addField('*:syringe: Toplam İyileşen:*',`\`\`〘 → ${body.countrydata[0].total_recovered} ← 〙\`\` `, true)
+    .addField('*:dna: Toplam Enfekte:*',`\`\`〘 → ${body.countrydata[0].total_active_cases} ← 〙\`\` `, true)
+    .addField('*:skull_crossbones: Toplam Ölümler:*',`\`\`〘 → ${body.countrydata[0].total_deaths} ← 〙\`\` `, true)
+    .addField('*:test_tube: Bugünki Vakalar:*',`\`\`〘 → ${body.countrydata[0].total_new_cases_today} ← 〙\`\` `, true)
+    .addField('*:warning: Bugünki Ölümler:*',`\`\`〘 → ${body.countrydata[0].total_new_deaths_today} ← 〙\`\` `, true)
+    .addField('*:bangbang: Ciddi Vakalar:*',`\`\`〘 → ${body.countrydata[0].total_serious_cases} ← 〙\`\` `, true)
+    .addField('*:flag_white:  Ülke:*',`\`\`〘 → ${ülk} ← 〙\`\` `, true)
+    .addField('*:bust_in_silhouette:   Kullanıcı (sen):*',`\`\`〘 → ${message.author.username} ← 〙\`\` `, true)
     .setTimestamp()
-    .setFooter('COVID-19 ', client.user.avatarURL);
-    message.channel.send(zerocode);
+    .setFooter('COVID-19', client.user.avatarURL);
+    
+    message.channel.send(embed);
 };
 exports.conf = {
-  enabled: true, 
-  guildOnly: false, 
-  aliases: ["corona", "covid", "covid19"],
-  permLevel: 0 
+  enabled: true,
+  guildOnly: false,
+  aliases: ['corona', 'coronabilgi', 'corona-bilgi', 'korona', 'koronabilgi', 'korona-bilgi', 'virüs'],
+  permLevel: 0
 };
 
 exports.help = {
   name: 'korona',
-  description: '',
-  usage: ''
+  description: 's',
+  usage: 's'
 };
